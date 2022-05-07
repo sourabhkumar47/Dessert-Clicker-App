5c8bdf057233d9fcf34c6d294291b6791792c636
sourabhkumar47 <20010001030@gateway.edu.in>
2022-05-07 13:25:39 +0530

    added various logcat

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -36,10 +36,41 @@ class MainActivity : AppCompatActivity() {
 
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
+
+    //it show various activities
+    //when we use app
+    //it show result in logcat
+    //search for D/MainActivity
+
     override fun onStart() {
         super.onStart()
-        Log.d("TAG","onStart")
+        Log.d(TAG,"onStart Called")
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart Called")
     }
 
     /** Dessert Data **/
@@ -71,7 +102,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("onCreate ","onCreate")
+        Log.d("MainActivity", "onCreate Called")
         // Use Data Binding to get reference to the views
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
